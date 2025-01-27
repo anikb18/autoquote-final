0c8458a7e21e56d33612ea9169df5e9fb1f909a1
anikb18 <anikbeauchemin18@gmail.com>
2025-01-27 03:24:37 -0500

    deleted:    src/index.css 	modified:   src/main.tsx 	modified:   src/styles.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./styles.css";
 import i18n from "./i18n/config";
 import { I18nextProvider } from 'react-i18next';
 
